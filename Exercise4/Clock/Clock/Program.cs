73952ce73649f0e238756c75d07413857889da41
cjk <510112057@qq.com>
2020-03-13 10:46:23 +0800

    add thread to count and make the program more official

--- Exercise4/Clock/Clock/Program.cs
+++ Exercise4/Clock/Clock/Program.cs
@@ -3,42 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace Clock
 {
-    public delegate void ClockHandler(object sender,TimeArgs args);
+    public delegate void ClockHandler(object sender,TimeArgs args,TimeArgs args2);
     public class TimeArgs
     {
-        public string X { get; set; }
-        public string Y { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
     }
     public class Clock
     {
         public event ClockHandler Running;
-        public void Run(string x, string y)
+        public void Run(TimeArgs args, TimeArgs args2)
         {
-            TimeArgs args = new TimeArgs() { X = x, Y = y };
-            Console.WriteLine("Clock begin to run...");
-            Running(this,args);
+            Running(this,args,args2);
         }
-        public void Tick(object sender, TimeArgs args)
-        {
-            Console.WriteLine("Tik tok tik tok...");
-        }
-        public void Alarm(object sender, TimeArgs args)
-        {
-            Console.WriteLine($"Alarm is ringing, now is {args.X}:{args.Y} !");
-        }
+
     }
 
     class Program
     {
+        public static void Tick(object sender, TimeArgs args, TimeArgs args2)
+        {
+            Console.WriteLine("Tik tok tik tok...");
+        }
+        public static void Alarm(object sender, TimeArgs args,TimeArgs args2)
+        {
+            int h = args2.X;
+            int m = args2.Y;
+            if(args.X==h&&args.Y==m)
+                Console.WriteLine($"Alarm is ringing, now is {h}:{m} !");
+        }
         static void Main(string[] args)
         {
             Clock clock = new Clock();
-            clock.Running += clock.Tick;
-            clock.Running += clock.Alarm;
-            clock.Run("18", "00");
+            clock.Running += Tick;
+            clock.Running += Alarm;
+            int terminate_x=2;
+            int terminate_y=10;
+            TimeArgs t=new TimeArgs() { X = terminate_x, Y = terminate_y };
+            for (int h=0;h<24;h++)
+            {
+                for(int i=0;i<60;i++)
+                {
+                    TimeArgs a = new TimeArgs() { X = h, Y = i };
+                    clock.Run(a,t);
+                    Thread.Sleep(1000);
+                    if (i == 59)
+                        i = 0;
+                }
+                if(h==23)
+                {
+                    h = 0;
+                }
+            }
+
             Console.ReadLine();
         }
     }
