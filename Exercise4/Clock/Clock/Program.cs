cc053fecb0a5e4517ec390254366804b5ea16c29
cjk <510112057@qq.com>
2020-03-13 09:40:25 +0800

    Exercise4 is finished

---
+++ Exercise4/Clock/Clock/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    public delegate void ClockHandler(object sender,TimeArgs args);
+    public class TimeArgs
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+    public class Clock
+    {
+        public event ClockHandler Running;
+        public void Run(string x, string y)
+        {
+            TimeArgs args = new TimeArgs() { X = x, Y = y };
+            Console.WriteLine("Clock begin to run...");
+            Running(this,args);
+        }
+        public void Tick(object sender, TimeArgs args)
+        {
+            Console.WriteLine("Tik tok tik tok...");
+        }
+        public void Alarm(object sender, TimeArgs args)
+        {
+            Console.WriteLine($"Alarm is ringing, now is {args.X}:{args.Y} !");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            clock.Running += clock.Tick;
+            clock.Running += clock.Alarm;
+            clock.Run("18", "00");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise4/ForEach/ForEach/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = null;
+            head = tail;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail==null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> p = head;
+            while(p!=null)
+            {
+                action(p.Data);
+                p = p.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intList = new GenericList<int>();
+            int sum = 0;
+            intList.Add(1);
+            intList.Add(15);
+            intList.Add(7);
+            intList.Add(0);
+            Console.WriteLine("遍历");
+            intList.ForEach(m =>
+            {
+                Console.Write(m+" ");
+            });
+            Console.WriteLine("遍历结束");
+            int max = intList.Head.Data;
+            int min = intList.Head.Data;
+            intList.ForEach(m =>
+            {
+                if(m>max)
+                {
+                    max = m;
+                }
+            });
+            Console.WriteLine("max=" + max);
+            intList.ForEach(m =>
+            {
+                if (m <min)
+                {
+                    min = m;
+                }
+            });
+            Console.WriteLine("min=" + min);
+            intList.ForEach(m =>
+            {
+                sum += m;
+            });
+            Console.WriteLine("sum=" + sum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
