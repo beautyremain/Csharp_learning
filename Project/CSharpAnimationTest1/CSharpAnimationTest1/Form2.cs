8af28b215f42658d27181cbfbadfa187ad868247
beautyremain <510112057@qq.com>
2020-05-07 22:43:08 +0800

    test1 finish

---
+++ Project/CSharpAnimationTest1/CSharpAnimationTest1/Form2.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpAnimationTest1
+{
+
+    public partial class Form2 : Form
+    {
+        public Basic basic;
+        List<ListItem> items = new List<ListItem>();
+        List<PictureBox> pictureBoxes = new List<PictureBox>();
+        List<AngleCollection> historys = new List<AngleCollection>();
+        public Form2()
+        {
+            InitializeComponent();
+            //button2.Click += button1_Click;
+            basic = new Basic();
+            for(int i=0;i<5;i++)
+            {
+                historys.Add(new AngleCollection(0, 0, 0));
+            }
+            foreach (Control control in this.Controls)
+            {
+                if(control is PictureBox)
+                {
+                    pictureBoxes.Add((PictureBox)control);
+
+                }
+            }
+
+            items.Add(new ListItem("0", "P0"));
+            items.Add(new ListItem("1", "P1"));
+            items.Add(new ListItem("2", "P2"));
+            items.Add(new ListItem("3", "P3"));
+            items.Add(new ListItem("4", "P4"));
+            //items.Add(new ListItem("5", "Item_5_Text"));
+            comboBox1.DisplayMember = "Text";        //显示
+            comboBox1.ValueMember = "Value";        //值
+            comboBox1.DataSource = items;        //绑定数据
+            label_AM.Text = basic.available.A.ToString();
+            label_BM.Text = basic.available.B.ToString();
+            label_CM.Text = basic.available.C.ToString();
+            trackBarA.Maximum = basic.available.A;
+            trackBarB.Maximum = basic.available.B;
+            trackBarC.Maximum = basic.available.C;
+            init_draw();
+        }
+        private static void activePieEvent(Graphics graphics, SolidBrush solidBrush, SolidBrush solidBrush2, Rectangle rectangle, float angle1, float angle2, Timer timer, ref int Tick)
+        {
+            //Console.WriteLine("被调用");
+            if (Tick > angle2)
+            {
+                timer.Stop();
+                //Console.WriteLine("Finish");
+                return;
+            }
+            if (Tick < 270-angle1)
+                graphics.FillPie(solidBrush, rectangle, angle1 + Tick, angle2 > Tick + 5 ? 5 : angle2 - Tick);
+            else
+                graphics.FillPie(solidBrush2, rectangle, angle1 + Tick, angle2 > Tick + 5 ? 5 : angle2 - Tick);
+            Tick += 5;
+        }
+        public static void activeDrawPie(Graphics graphics, SolidBrush solidBrush, SolidBrush solidBrush2, Rectangle rectangle, float angle1, float angle2, int interval)
+        {
+            Timer timer = new Timer();
+            timer.Interval = interval;
+            int Tick = 0;
+            timer.Tick += new EventHandler((o, e) => activePieEvent(graphics, solidBrush, solidBrush2, rectangle, angle1, angle2, timer, ref Tick));
+            timer.Start();
+        }
+        public void init_draw()
+        {
+            SolidBrush fix_brush = new SolidBrush(Color.LightGreen);
+            SolidBrush change_brush = new SolidBrush(Color.LightSalmon);
+            SolidBrush warn_brush = new SolidBrush(Color.Red);
+            Rectangle rectangle1 = new Rectangle(10, 10, 100, 100);
+            Rectangle rectangle2 = new Rectangle(150, 10, 100, 100);
+            Rectangle rectangle3 = new Rectangle(290, 10, 100, 100);
+            for (int i = 0; i < 5; i++)
+            {
+                Resourse Max = basic.ProcessList[i].Max;
+                Resourse Allocation = basic.ProcessList[i].Allocation;
+
+                //Graphics g = pictureBoxes[4 - i].CreateGraphics();
+                Bitmap img = new Bitmap(pictureBoxes[i].Width, pictureBoxes[i].Height);
+                pictureBoxes[ i].Image = img;
+                Graphics g = Graphics.FromImage(img);
+                g.Clear(Color.White);
+                //Graphics g = pictureBox.CreateGraphics();
+                g.FillPie(fix_brush, rectangle1, 0, 360);
+                if(Max.A==0)
+                    g.FillPie(change_brush, rectangle1, 0, 360);
+                g.DrawString(Allocation.A.ToString()+"/"+Max.A.ToString(), new Font("等线", 14), new SolidBrush(Color.Black), rectangle1.X+30, 115);
+                g.FillPie(fix_brush, rectangle2, 0, 360);
+                if (Max.B == 0)
+                    g.FillPie(change_brush, rectangle2, 0, 360);
+                g.DrawString(Allocation.B.ToString() + "/" + Max.B.ToString(), new Font("等线", 14), new SolidBrush(Color.Black), rectangle2.X + 30, 115);
+                g.FillPie(fix_brush, rectangle3, 0, 360);
+                if (Max.C == 0)
+                    g.FillPie(change_brush, rectangle3, 0, 360);
+                g.DrawString(Allocation.C.ToString() + "/" + Max.C.ToString(), new Font("等线", 14), new SolidBrush(Color.Black), rectangle3.X + 30, 115);
+
+
+            }
+        }
+            private void button1_Click(object sender, EventArgs e)
+        {
+            basic.printPro(basic.ProcessList, basic.available);
+            SolidBrush fix_brush = new SolidBrush(Color.LightGreen);
+            SolidBrush change_brush = new SolidBrush(Color.LightSalmon);
+            SolidBrush warn_brush = new SolidBrush(Color.Red);
+            Rectangle rectangle1 = new Rectangle(10, 10, 100, 100);
+            Rectangle rectangle2 = new Rectangle(150, 10, 100, 100);
+            Rectangle rectangle3 = new Rectangle(290, 10, 100, 100);
+            for (int i = 0; i < 5; i++)
+            {
+                AngleCollection history = historys[i];
+                Resourse Max=basic.ProcessList[i].Max;
+                Resourse Allocation = basic.ProcessList[i].Allocation;
+                float angleA = Allocation.A * 360 / (Max.A == 0 ? 1 : Max.A);
+                float angleB = Allocation.B * 360 / (Max.B == 0 ? 1 : Max.B);
+                float angleC = Allocation.C * 360 / (Max.C == 0 ? 1 : Max.C);
+                //this.listBox1.Items.Add(angleC);
+                Graphics g=pictureBoxes[i].CreateGraphics();
+                //Bitmap img = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                //pictureBoxes[4 - i].Image = img;
+                //Graphics g = Graphics.FromImage(img);
+                //pictureBoxes[4 - i].Image= getBitMapFile(pictureBox1.Width, pictureBox1.Height);
+               // g.FillPie(fix_brush, rectangle1, 0, 360);
+                activeDrawPie(g,change_brush,warn_brush, rectangle1, -90+history.A, angleA - history.A,100);
+                g.FillRectangle(new SolidBrush(Color.White), rectangle1.X, 115, 100, 20);
+                g.DrawString(Allocation.A.ToString() + "/" + Max.A.ToString(), new Font("等线", 14), new SolidBrush(Color.Black), rectangle1.X + 30, 115);
+                //g.FillPie(fix_brush, rectangle2, 0, 360);
+                activeDrawPie(g, change_brush, warn_brush, rectangle2, -90+history.B, angleB - history.B, 100);
+                g.FillRectangle(new SolidBrush(Color.White), rectangle2.X, 115, 100, 20);
+                g.DrawString(Allocation.B.ToString() + "/" + Max.B.ToString(), new Font("等线", 14), new SolidBrush(Color.Black), rectangle2.X + 30, 115);
+                //g.FillPie(fix_brush, rectangle3, 0, 360);
+                activeDrawPie(g, change_brush, warn_brush, rectangle3, -90+history.C, angleC - history.C,100);
+                g.FillRectangle(new SolidBrush(Color.White), rectangle3.X, 115, 100, 20);
+                g.DrawString(Allocation.C.ToString() + "/" + Max.C.ToString(), new Font("等线", 14), new SolidBrush(Color.Black), rectangle3.X + 30, 115);
+                history.A = angleA;
+                history.B = angleB;
+                history.C = angleC;
+
+
+            }
+              //panels[3].CreateGraphics().DrawLine(new Pen(Color.Blue,10), 1, 1, 100, 100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(basic.addRequest(trackBarA.Value,trackBarB.Value,trackBarC.Value, comboBox1.SelectedItem.ToString()))
+            {
+                trackBarA.Maximum = basic.available.A;
+                trackBarB.Maximum = basic.available.B;
+                trackBarC.Maximum = basic.available.C;
+                label_AM.Text = basic.available.A.ToString();
+                label_BM.Text = basic.available.B.ToString();
+                label_CM.Text = basic.available.C.ToString();
+                button1_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("请重新分配资源","分配失败");
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label_Bval.Text=trackBarB.Value.ToString();
+        }
+
+        private void trackBarA_ValueChanged(object sender, EventArgs e)
+        {
+            label_Aval.Text = trackBarA.Value.ToString();
+        }
+
+        private void trackBarC_ValueChanged(object sender, EventArgs e)
+        {
+            label_Cval.Text = trackBarC.Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+              //listBox1.Items.Add( comboBox1.SelectedItem.ToString());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is TrackBar)
+                {
+                    TrackBar tb = (TrackBar)control;
+                    tb.Value = 0;
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            foreach(var pb in pictureBoxes)
+            {
+                listBox1.Items.Add(pb.Name);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            basic.addRequest(1, 1, 1, "P4");
+            basic.addRequest(1, 1, 1, "P0");
+            Form3 form3 = new Form3(this.basic,new Resourse(trackBarA.Value, trackBarB.Value, trackBarC.Value), comboBox1.SelectedItem.ToString());
+            form3.Show();
+        }
+    }
+    public class AngleCollection
+    {
+        public float A { get; set; }
+        public float B { get; set; }
+        public float C { get; set; }
+        public AngleCollection(float a, float b, float c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
---
+++ Project/CSharpAnimationTest1/CSharpAnimationTest1/Form3.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace CSharpAnimationTest1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_avaiA = new System.Windows.Forms.Label();
+            this.label_avaiC = new System.Windows.Forms.Label();
+            this.label_avaiB = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("宋体", 20F);
+            this.label13.Location = new System.Drawing.Point(406, 8);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 27);
+            this.label13.TabIndex = 52;
+            this.label13.Text = "C";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("宋体", 20F);
+            this.label12.Location = new System.Drawing.Point(270, 8);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(26, 27);
+            this.label12.TabIndex = 51;
+            this.label12.Text = "B";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("宋体", 20F);
+            this.label11.Location = new System.Drawing.Point(127, 8);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 27);
+            this.label11.TabIndex = 50;
+            this.label11.Text = "A";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("宋体", 20F);
+            this.label10.Location = new System.Drawing.Point(18, 239);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 27);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "P1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("宋体", 20F);
+            this.label9.Location = new System.Drawing.Point(11, 409);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 27);
+            this.label9.TabIndex = 48;
+            this.label9.Text = "P2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 20F);
+            this.label8.Location = new System.Drawing.Point(11, 579);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 27);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "P3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 20F);
+            this.label7.Location = new System.Drawing.Point(11, 747);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 27);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "P4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 20F);
+            this.label4.Location = new System.Drawing.Point(13, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 27);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "P0";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox6.Location = new System.Drawing.Point(68, 39);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(405, 148);
+            this.pictureBox6.TabIndex = 57;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox5.Location = new System.Drawing.Point(68, 209);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(405, 155);
+            this.pictureBox5.TabIndex = 56;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox4.Location = new System.Drawing.Point(68, 382);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(405, 149);
+            this.pictureBox4.TabIndex = 55;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(68, 537);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(405, 147);
+            this.pictureBox3.TabIndex = 54;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(68, 701);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(405, 140);
+            this.pictureBox1.TabIndex = 53;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(505, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(371, 268);
+            this.listBox1.TabIndex = 58;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(801, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 59;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F);
+            this.label1.Location = new System.Drawing.Point(772, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 27);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 20F);
+            this.label2.Location = new System.Drawing.Point(697, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 27);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 20F);
+            this.label3.Location = new System.Drawing.Point(621, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 27);
+            this.label3.TabIndex = 60;
+            this.label3.Text = "A";
+            // 
+            // label_avaiA
+            // 
+            this.label_avaiA.AutoSize = true;
+            this.label_avaiA.Font = new System.Drawing.Font("宋体", 30F);
+            this.label_avaiA.Location = new System.Drawing.Point(39, 33);
+            this.label_avaiA.Name = "label_avaiA";
+            this.label_avaiA.Size = new System.Drawing.Size(37, 40);
+            this.label_avaiA.TabIndex = 65;
+            this.label_avaiA.Text = "C";
+            // 
+            // label_avaiC
+            // 
+            this.label_avaiC.AutoSize = true;
+            this.label_avaiC.Font = new System.Drawing.Font("宋体", 30F);
+            this.label_avaiC.Location = new System.Drawing.Point(190, 33);
+            this.label_avaiC.Name = "label_avaiC";
+            this.label_avaiC.Size = new System.Drawing.Size(37, 40);
+            this.label_avaiC.TabIndex = 64;
+            this.label_avaiC.Text = "B";
+            // 
+            // label_avaiB
+            // 
+            this.label_avaiB.AutoSize = true;
+            this.label_avaiB.Font = new System.Drawing.Font("宋体", 30F);
+            this.label_avaiB.Location = new System.Drawing.Point(115, 33);
+            this.label_avaiB.Name = "label_avaiB";
+            this.label_avaiB.Size = new System.Drawing.Size(37, 40);
+            this.label_avaiB.TabIndex = 63;
+            this.label_avaiB.Text = "A";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("宋体", 20F);
+            this.label15.Location = new System.Drawing.Point(491, 382);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(201, 27);
+            this.label15.TabIndex = 66;
+            this.label15.Text = "总资源剩余量：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_avaiA);
+            this.groupBox1.Controls.Add(this.label_avaiB);
+            this.groupBox1.Controls.Add(this.label_avaiC);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(580, 458);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(265, 100);
+            this.groupBox1.TabIndex = 67;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 883);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_avaiA;
+        private System.Windows.Forms.Label label_avaiC;
+        private System.Windows.Forms.Label label_avaiB;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
